a31fa327fa341dd2395a668e853fd80e4b5feebc
settipallesaiganesh-spec <settipallesaiganesh@gmail.com>
2026-09-14 08:54:48 +0530

    Add: car-pooling - submission-1

---
+++ Data Structures & Algorithms/car-pooling/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        changes=[0]*1001
+        for passengers, start, end in trips:
+            changes[start]+=passengers
+            changes[end]-=passengers
+        current=0
+        for i in range(1001):
+            current+= changes[i]
+            if current>capacity:
+                return False
+        return True
+        #Time and space complexity is O(n) and O(1) respectively
